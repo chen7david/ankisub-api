b8b06565d2032a2e888dba815f67a9e3bbe45bbf
David Chen <chen7david@me.com>
2025-10-16 21:28:55 -0400

    chore: implement a test producer worker

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.post("/enqueue", async (c) => {
+  const body = await c.req.json();
+  await c.env.ANKISUB_QUEUE.send(body);
+  return c.json({ status: "Message enqueued" });
+});
+
 export default app;
